26de2daaad148789fb9bae75d4bbec4f69e75c3b
Replit user <>
2023-05-11 23:10:20 +0000

    new try

--- heap.c
+++ heap.c
@@ -48,9 +48,7 @@ void heap_push(Heap* pq, void* data, int priority){
 
 void heap_pop(Heap* pq){
   heapElem aux;
-  
-  aux = pq -> heapArray[pq->size-1];
-  pq -> heapArray[0] = aux;
+  pq -> heapArray[0] = pq -> heapArray[pq->size-1];;
   pq -> size--; 
   
   if(pq -> size == 1){
